2b1a477a01ee2a63b6746a1b45f8e2f9aa905cc8
Anthony <anthony.diaz.yacsahuanga@gmail.com>
2026-02-02 01:17:35 -0500

    feature new register in user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,34 +4,25 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
-
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectRepository(User)
-    private readonly userRepo: Repository<User>,
+    @InjectRepository(User) private readonly userRepo: Repository<User>,
   ) {}
-
   createUser(createUserDto: CreateUserDto) {
     const newUser = this.userRepo.create(createUserDto);
     const user = this.userRepo.save(newUser);
     return user;
   }
-
   async findAllUsers() {
     const users = await this.userRepo.find();
     return users;
   }
-
   async findOneUser(id: string) {
     const user = await this.userRepo.findOneBy({ id });
 
-    if (!user) throw new NotFoundException(`User with ID ${id} not found`)
-
-    const { name, last_name, grade } = user
-
-    return {
-      name, last_name, grade
-    };
+    if (!user) throw new NotFoundException(`User with ID ${id} not found`);
+    const { name, grade, specialty, dni, cip, area } = user;
+    return { name, grade, specialty, dni, cip, area };
   }
 }
